35d55a0a51825e711c253a64d48bb725fd33b997
Luiz <luizantonio050206@gmail.com>
2025-09-21 15:52:00 -0300

    Update Entregas

---
+++ documentos/Entrega 1/Desenvolvimento Web FullStack/Backend/src/db.js
@@ -0,0 +1,13 @@
+
+import 'dotenv/config' // Carrega variáveis do .env
+import mysql from 'mysql2/promise' // Importa MySQL com suporte a Promises
+
+// Cria um pool de conexões com o banco de dados
+export const pool = await mysql.createPool({
+   host: process.env.MYSQL_HOST,
+   user: process.env.MYSQL_USER, 
+   password: process.env.MYSQL_PASSWORD, 
+   database: process.env.MYSQL_DB,
+   waitForConnections: true,
+   connectionLimit: 10 
+})
