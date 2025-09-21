a13b4b5307873c771e75b37e9a3fc7f0cbc0b581
Endrew <168784380+EndrewFMA@users.noreply.github.com>
2025-09-21 19:49:44 -0300

    Estruturação das pastas
    Troquei de lugar as pastas back e front e exclui arquivos desnecessários

--- Backend/src/server.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0' // Ignora certificados SSL inválidos (apenas para testes locais)
-
-import 'dotenv/config'
-import app from './app.js'
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Servidor OnFire em http://localhost:${port}`)
-})
